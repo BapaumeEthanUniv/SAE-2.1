40d763afda5ead629b52d986732b06f7f893e7b9
Alexis Coffard <alexis.coffard@etu.univ-lehavre.fr>
2026-06-04 14:53:50 +0200

    Modification de FrameSaisie

--- ihm/FrameSaisie.java
+++ ihm/FrameSaisie.java
@@ -4,24 +4,44 @@ import controleur.Controleur;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class FrameSaisie extends JFrame
 {
-	private PanelSaisie panelSaisie;
+	private JPanel[] tabPanel;
+
+	private JPanel panelActif;
 	
 	public FrameSaisie (Controleur ctrl)
 	{
 		this.setTitle("Saisie des informations");
-		this.setSize(500, 400); 
+		this.setSize(600, 650); 
 		this.setLayout(new BorderLayout());
 		
-		this.panelSaisie = new PanelSaisie(ctrl);
+		this.tabPanel	 = new JPanel[4];
 		
-		this.add(this.panelSaisie, BorderLayout.CENTER);
+		this.tabPanel[0] = new PanelSaisie(ctrl, this, 0);
+		this.tabPanel[1] = new PanelZone  (ctrl, this, 1);
+		//this.tabPanel[2] = new PanelSymbole(this, 2);
+
+		this.panelActif = this.tabPanel[0];
+		
+		this.add(this.panelActif, BorderLayout.CENTER);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         	this.setVisible(true); 
 	}
+	
+	public void setPnl(JPanel pnl)
+	{
+		this.remove(this.panelActif);
+		this.panelActif = pnl;
+		this.add(this.panelActif);
+		
+		this.repaint();
+		this.revalidate();
+	}
+	
+	public JPanel getPnl(int i) { return this.tabPanel[i]; }
 }
 
