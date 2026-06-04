c287f16647cb313a823c0c0c198b9c37477a11e0
Kiwitur <arthurkuroko@gmail.com>
2026-06-04 09:15:51 +0200

    ajout du changement d'image

--- ihm/PanelCarte.java
+++ ihm/PanelCarte.java
@@ -1,51 +1,64 @@
+package ihm;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class PanelCarte extends JPanel
+public class PanelCarte extends JPanel implements ActionListener
 {
-	// private Controleur     ctrl;
-	private ImageIcon imgPersonnage;
+    private ImageIcon imgPersonnage;
+    private JButton   btnChangerImage;
+    private Image     imageRedimensionnee;
+    private ImageIcon iconeFinale;
+    private JLabel    label;
+
+    public PanelCarte()
+    {
+        JPanel panelAfficheCarte;
+        JPanel panelAfficheScore;
+        int hauteur = 500;
+        int largeur = 350;
+
+        this.setLayout(new BorderLayout());
+
+        int image = (int)(Math.random() * 10) + 1;
+
+        panelAfficheCarte = new JPanel(new GridLayout());
+        panelAfficheScore = new JPanel(new GridLayout(2, 2));
+
+        this.imgPersonnage = new ImageIcon("../images/" + image + ".png");
+        this.imageRedimensionnee = this.imgPersonnage.getImage()
+                .getScaledInstance(largeur, hauteur, Image.SCALE_SMOOTH);
+        this.iconeFinale = new ImageIcon(this.imageRedimensionnee);
+        this.label = new JLabel(this.iconeFinale);
 
 
-	public PanelCarte()
-	{
-		JPanel panelAfficheCarte;
-		JPanel panelAfficheScore;
+        this.btnChangerImage = new JButton("Changer l'image");
 
-		int hauteur = 500;
-		int largeur = 350;
-		int image;
+        panelAfficheCarte.setOpaque(false);
+        panelAfficheCarte.add(this.label);
 
-		// this.ctrl = ctrl;
-		this.setLayout(new BorderLayout());
+        panelAfficheScore.add(new JLabel("manche : "));
+        panelAfficheScore.add(new JLabel("tour : "));
+        panelAfficheScore.add(new JLabel("score joueur : "));
 
-		image = (int)(Math.random() * 10) + 1;
+        this.add(panelAfficheCarte, BorderLayout.CENTER);
+        this.add(panelAfficheScore, BorderLayout.SOUTH);
+        this.add(this.btnChangerImage, BorderLayout.NORTH);
 
-		panelAfficheCarte = new JPanel(new GridLayout());
-		panelAfficheScore = new JPanel(new GridLayout(2,2));
+        this.btnChangerImage.addActionListener(this);
+    }
 
-		this.imgPersonnage = new ImageIcon( "../images/" + image + ".png" );
-
-		Image imageRedimensionnee = this.imgPersonnage.getImage().getScaledInstance(largeur, hauteur, Image.SCALE_SMOOTH);
-
-		ImageIcon iconeFinale = new ImageIcon(imageRedimensionnee);
-
-		JLabel label = new JLabel(iconeFinale);
-
-		panelAfficheCarte.setOpaque(false);
-
-		panelAfficheCarte.add(label);
-
-		panelAfficheScore.add(new JLabel("manche : "));
-		panelAfficheScore.add(new JLabel("tour : "));
-		panelAfficheScore.add(new JLabel("score joueur : "));
-
-		this.add(panelAfficheCarte, BorderLayout.CENTER);
-		this.add(panelAfficheScore, BorderLayout.SOUTH);
-	}
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        int image = ((int)(Math.random() * 10) + 1);
+        this.imgPersonnage = new ImageIcon("../images/" + image + ".png");
+        this.imageRedimensionnee = this.imgPersonnage.getImage()
+                .getScaledInstance(350, 500, Image.SCALE_SMOOTH);
+        this.iconeFinale = new ImageIcon(this.imageRedimensionnee);
+        this.label.setIcon(this.iconeFinale);
+    }
 }
