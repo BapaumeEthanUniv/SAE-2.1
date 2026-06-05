933a3e2292f39a161f83441e54faf4bb6b7f4223
Alexis Coffard <alexis.coffard@etu.univ-lehavre.fr>
2026-06-05 13:36:53 +0200

    ajout ctrl

--- controleur/Controleur.java
+++ controleur/Controleur.java
@@ -4,6 +4,7 @@ import ihm.FrameCreation;
 
 import metier.PlateauJeu;
 import metier.Casting;
+import metier.Couleur;
 import metier.CreateurPlateau;
 import metier.Role;
 import metier.Zone;
@@ -36,6 +37,22 @@ public class Controleur
 	{
 		return Role.valueOf    ( indice ).getLibelle();
 	}
+
+	//page 1
+	public void initCreateur(String nomPlateau, int nbLigne, int nbColonne, int tailleCase, Role[] tabRole, Casting[] tabCasting)
+	{
+		this.metier = new CreateurPlateau(nomPlateau, nbLigne, nbColonne, tailleCase, tabRole, tabCasting);
+	}
+
+	//page 2
+	public boolean modifierZone(int lig, int col, Zone zone) {return this.metier.modifierZone(lig, col, zone);}
 	
+	public boolean changerCouleurZone(Couleur couleur, Zone zone) {return this.metier.changerCouleurZone(couleur, zone);}
+
+	public void zonePrecedente() {this.metier.zonePrecedente();}
+
+	public void nouvelleZone() {this.metier.nouvelleZone();}
+	
+
 	public static void main (String[] args) { new Controleur(); }
 }
