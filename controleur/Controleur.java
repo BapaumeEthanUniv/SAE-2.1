dd93e813437ffee4cecf6d299d89527c12427c01
Alexis Coffard <alexis.coffard@etu.univ-lehavre.fr>
2026-06-06 18:47:14 +0200

    Ajout getters

--- controleur/Controleur.java
+++ controleur/Controleur.java
@@ -33,8 +33,10 @@ public class Controleur
 	public ArrayList<Zone> getLstZones() {return metier.getLstZones();} //listes des zones créées
 	public Zone     getZoneActive() {return metier.getZoneActive();}
 
-	public int getNbLigne()   { return metier.getNbLigne();}
+    public String getNomPlateau() { return metier.getNomPlateau(); }
+    public int getNbLigne()   { return metier.getNbLigne();}
 	public int getNbColonne() { return metier.getNbColonne();}
+    public int getTailleCase() { return metier.getTailleCase(); }
 
 	public Role[]   getTabRole()     {return this.metier.getTabRole();}
 	public Casting[] getTabCasting() {return this.metier.getTabCasting();}
