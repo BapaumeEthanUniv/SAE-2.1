066c9000f3e5687752c55acd0d12441dc6a5acd3
Alexis Coffard <alexis.coffard@etu.univ-lehavre.fr>
2026-06-06 20:39:39 +0200

    j'ai essayé de faire le truc de zone, mais pas réussi à revoir

--- ihm/PanelZone.java
+++ ihm/PanelZone.java
@@ -28,6 +28,7 @@ public class PanelZone extends JPanel implements ActionListener
 
     private Image 		        imgFond;
 
+    private JToggleButton       tglGomme;
     private JButton     		btnSuivant;
     private JButton     		btnPrecedent;
     private JButton     		btnZonePrecedent;
@@ -85,13 +86,17 @@ public class PanelZone extends JPanel implements ActionListener
         JPanel pnlBtnSuite      = new JPanel(new GridLayout(2,2, 5, 5)); // 5px d'espace entre les boutons
         pnlBtnSuite             .setOpaque(false);
 
+        JPanel pnlHaut          = new JPanel();
+        pnlHaut                 .setLayout(new BorderLayout());
+        pnlHaut                 .setOpaque(false);
+
         JPanel pnlBandeau       = new JPanel(new GridLayout(2,1, 5, 5));
         pnlBandeau              .setOpaque(false);
 
         JPanel pnlTitre         = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
         pnlTitre                .setOpaque(false);
 
-        JPanel pnlChoixCouleur  = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        JPanel pnlChoixCouleur  = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
         pnlChoixCouleur         .setOpaque(false);
 
         JPanel pnlConteneur     = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 30));
@@ -103,6 +108,10 @@ public class PanelZone extends JPanel implements ActionListener
 
         JPanel pnlGrille        = new JPanel(new GridLayout(this.tailleLargeur, this.tailleHauteur, 2, 2));
         pnlGrille               .setBackground(new Color(60, 60, 75));
+
+        JSeparator separateur   = new JSeparator(SwingConstants.HORIZONTAL);
+        separateur              .setForeground(new Color(150, 150, 150));
+        separateur              .setBackground(new Color(0, 0, 0, 0));
 
         for (int lig = 0; lig < this.tailleLargeur; lig++)
         {
@@ -152,31 +161,44 @@ public class PanelZone extends JPanel implements ActionListener
             }
         }
 
-        JLabel lblChoix = new JLabel("Couleur :");
-        lblChoix.setFont(this.policeBandeau);
+        JLabel lblChoix         = new JLabel("Couleur :");
+        lblChoix                .setFont(this.policeBandeau);
 
-        this.jcbCouleur = new JComboBox<>(Couleur.values());
-        this.jcbCouleur.setSelectedIndex(12); // Saumon (couleur par défaut)
-        this.jcbCouleur.setFont(new Font("SansSerif", Font.PLAIN, 13));
-        this.jcbCouleur.setFocusable(false);
-        this.jcbCouleur.setOpaque(false);
-        this.jcbCouleur.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        this.jcbCouleur.setPreferredSize(new Dimension(450, 30));
+        this.jcbCouleur         = new JComboBox<>(Couleur.values());
+        this.jcbCouleur         .setSelectedIndex(11); // Saumon (couleur par défaut)
+        this.jcbCouleur         .setFont(new Font("SansSerif", Font.PLAIN, 13));
+        this.jcbCouleur         .setFocusable(false);
+        this.jcbCouleur         .setOpaque(false);
+        this.jcbCouleur         .setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        this.jcbCouleur         .setPreferredSize(new Dimension(300, 30));
 
-        this.btnSuivant       = new JButton("Suivant >>");
-        this.btnPrecedent     = new JButton("<< Précédent");
-        this.btnZonePrecedent = new JButton("Zone précédente");
-        this.btnZoneSuivant   = new JButton("Nouvelle Zone");
+        this.tglGomme           = new JToggleButton("");
+        this.tglGomme           .setFocusable(false);
+        this.tglGomme           .setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+        ImageIcon iconeOriginale = new ImageIcon("./images/gomme.png");
+        Image imageRedimensionnee = iconeOriginale.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+        this.tglGomme.setIcon(new ImageIcon(imageRedimensionnee));
+
+        this.btnSuivant         = new JButton("Suivant >>");
+        this.btnPrecedent       = new JButton("<< Précédent");
+        this.btnZonePrecedent   = new JButton("Zone précédente");
+        this.btnZoneSuivant     = new JButton("Nouvelle Zone");
 
         /* ------------------------------ */
         /* Positionnement des Composants  */
         /* ------------------------------ */
         pnlTitre.add(lblTitre);
+
         pnlChoixCouleur.add(lblChoix);
         pnlChoixCouleur.add(this.jcbCouleur);
+        pnlChoixCouleur.add(this.tglGomme);
 
         pnlBandeau.add(pnlTitre);
         pnlBandeau.add(pnlChoixCouleur);
+
+        pnlHaut.add(pnlBandeau, BorderLayout.CENTER);
+        pnlHaut.add(separateur, BorderLayout.SOUTH);
 
         pnlConteneur.add(pnlGrille);
 
@@ -185,7 +207,7 @@ public class PanelZone extends JPanel implements ActionListener
         pnlBtnSuite.add(this.btnPrecedent);
         pnlBtnSuite.add(this.btnSuivant);
 
-        this.add(pnlBandeau, BorderLayout.NORTH);
+        this.add(pnlHaut        , BorderLayout.NORTH);
         this.add(pnlConteneur   , BorderLayout.CENTER);
         this.add(pnlBtnSuite    , BorderLayout.SOUTH);
 
@@ -230,14 +252,42 @@ public class PanelZone extends JPanel implements ActionListener
             if (choix != null)
             {
                 this.couleurActuelle = choix.getCouleur();
+
+                if (this.ctrl.getZoneActive() != null)
+                {
+                    this.ctrl.changerCouleurZone(choix, this.ctrl.getZoneActive());
+                }
             }
         }
     }
 
     private void colorierCase(int lig, int col)
     {
-        tabPanelCases[lig][col].setBackground(couleurActuelle);
-        tabPanelCases[lig][col].repaint();
+        Zone    zoneActuelle;
+        boolean placementValide;
+
+        if (this.tglGomme.isSelected())
+        {
+            this.ctrl.effacerZone(lig, col);
+            tabPanelCases[lig][col].setBackground(Color.WHITE);
+            tabPanelCases[lig][col].repaint();
+            return;
+        }
+
+        zoneActuelle = this.ctrl.getZoneActive();
+        if (zoneActuelle == null) { return; }
+
+        placementValide = this.ctrl.modifierZone(lig, col, zoneActuelle);
+
+        if (placementValide)
+        {
+            tabPanelCases[lig][col].setBackground(couleurActuelle);
+            tabPanelCases[lig][col].repaint();
+        }
+        else
+        {
+            System.out.println("zone placé refusé."); // A changer par un JLabel ou autre
+        }
     }
 
     @Override
