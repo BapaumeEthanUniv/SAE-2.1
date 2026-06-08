c9e36a140acecada1cb70e960d377cf78c79a37c
Alexis Coffard <alexis.coffard@etu.univ-lehavre.fr>
2026-06-08 13:47:10 +0200

    modif quelques couleurs pour nuancer

--- metier/Couleur.java
+++ metier/Couleur.java
@@ -5,19 +5,18 @@ import java.awt.Color;
 public enum Couleur
 {
 	ROSE		( "Rose"	, new Color (230,100,160)),
-	ROUGE		( "Rouge"	, Color.RED		) ,
-	ROUGE_PRINCIPAL ( "Rouge Principal"	, new Color (255,64  ,64 )),
-	BORDEAUX	( "Bordeaux"	, new Color (109,7  ,26 )),
-	ORANGE 		( "Orange"  	, new Color (255,149,9  )),
-	JAUNE		( "Jaune"	, Color.YELLOW		) ,
-	VERT_CITRON ( "Vert Citron"	, new Color (140,210,40 )),
-	VERT		( "Vert"	, Color.GREEN		) ,
-	CYAN		( "Cyan"	, Color.CYAN		) ,
-	MAGENTA		( "Magenta"	, Color.MAGENTA		) ,
-	VIOLET		( "Violet"	, new Color (140,60 ,200)),
-	BLEU		( "Bleu"	, new Color (40 ,110,220)),
+	ROUGE 		( "Rouge"	, new Color (255,64 , 64)),
+	BORDEAUX	( "Bordeaux"	, new Color (109,  7, 26)),
+	ORANGE 		( "Orange"  	, new Color (255,149,  9)),
+	JAUNE		( "Jaune"	, new Color (255,228, 54)),
+	VERT_CITRON 	( "Vert Citron"	, new Color (140,210, 40)),
+	VERT		( "Vert"	, new Color ( 58,242, 75)),
+	TURQUOISE	( "Turquoise"	, new Color ( 37,253,233)),
+	BLEU		( "Bleu"	, new Color ( 40,110,220)),
+	MAUVE		( "Mauve"	, new Color (212,115,212)),
+	VIOLET		( "Violet"	, new Color (140, 60,200)),
 	SAUMON 		( "Saumon"	, new Color (240,150,120)),
-	MARRON 		( "Marron"	, new Color (140,80 ,30 )),
+	MARRON 		( "Marron"	, new Color (140, 80, 30)),
 	GRIS   		( "Gris"	, new Color (150,150,150));
 	
 	private String lib;
