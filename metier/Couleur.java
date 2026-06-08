15dd018c7635f0fefa0ef315b89c1b88813801ac
Armand Dupont <armand.dupont@etu.univ-lehafre.fr>
2026-06-08 14:06:04 +0200

    mise en forme de Couleur.java

--- metier/Couleur.java
+++ metier/Couleur.java
@@ -4,39 +4,53 @@ import java.awt.Color;
 
 public enum Couleur
 {
-	ROSE		( "Rose"	, new Color (230,100,160)),
-	ROUGE		( "Rouge"	, Color.RED		) ,
-	ROUGE_PRINCIPAL ( "Rouge Principal"	, new Color (255,64  ,64 )),
-	BORDEAUX	( "Bordeaux"	, new Color (109,7  ,26 )),
-	ORANGE 		( "Orange"  	, new Color (255,149,9  )),
-	JAUNE		( "Jaune"	, Color.YELLOW		) ,
-	VERT_CITRON ( "Vert Citron"	, new Color (140,210,40 )),
-	VERT		( "Vert"	, Color.GREEN		) ,
-	CYAN		( "Cyan"	, Color.CYAN		) ,
-	MAGENTA		( "Magenta"	, Color.MAGENTA		) ,
-	VIOLET		( "Violet"	, new Color (140,60 ,200)),
-	BLEU		( "Bleu"	, new Color (40 ,110,220)),
-	SAUMON 		( "Saumon"	, new Color (240,150,120)),
-	MARRON 		( "Marron"	, new Color (140,80 ,30 )),
-	GRIS   		( "Gris"	, new Color (150,150,150));
+	ROSE        ( "Rose"        , new Color (230,100,160)),
+	ROUGE       ( "Rouge"       , Color.RED) ,
+	ROUGE_CLAIR ( "Rouge Clair" , new Color (255,64 ,64 )),
+	BORDEAUX    ( "Bordeaux"    , new Color (109,7  ,26 )),
+	ORANGE      ( "Orange"      , new Color (255,149,9  )),
+	JAUNE       ( "Jaune"       , Color.YELLOW),
+	VERT_CITRON ( "Vert Citron" , new Color (140,210,40 )),
+	VERT        ( "Vert"        , Color.GREEN),
+	VERT_FONCE  ( "Vert Foncé"  , new Color (0  ,128,0  )),
+	CYAN        ( "Cyan"        , Color.CYAN) ,
+	MAGENTA     ( "Magenta"     , Color.MAGENTA) ,
+	VIOLET      ( "Violet"      , new Color (140,60 ,200)),
+	BLEU        ( "Bleu"        , new Color (40 ,110,220)),
+	BLEU_FONCE  ( "Bleu Foncé"  , new Color (0  ,0  ,255)),
+	SAUMON      ( "Saumon"      , new Color (240,150,120)),
+	MARRON      ( "Marron"      , new Color (140,80 ,30 )),
+	VIOLET_PALE ( "Violet Pâle" , new Color (230,180,255)),
+	BLEU_PALE   ( "Bleu Pâle"   , new Color (175,221,255)),
+	VERT_PALE   ( "Vert Pâle"   , new Color (175,255,190)),
+	ROSE_PALE   ( "Rose pale"   , new Color (255,180,240)),
+	BEIGE       ( "Beige"       , new Color (255,215,175)),
+	TURQUOISE   ( "turquoise"   , new Color (110,255,255)),
+	GRIS_CLAIR  ( "Gris clair"  , new Color (200,200,200)),
+	GRIS        ( "Gris"        , new Color (150,150,150));
 	
 	private String lib;
 	private Color couleur;
-	 
-	Couleur (String lib, Color couleur) { this.lib = lib; this.couleur = couleur; }
-	 
-	public Color getCouleur() {return couleur;}
-
-	public String getLib() {return lib;}
-
-	public String toString()
-	{
-		return this.name();
-	}
 	
+	Couleur (String lib, Color couleur) { this.lib = lib; this.couleur = couleur; }
+	
+	                /*----------*/
+	                /* Méthodes */
+	                /*----------*/
+	
+	//Geters :
+	public Color      getCouleur() {return couleur;}
+
+	public String     getLib()     {return lib;}
+
 	public static int getNbCouleur() 
 	{
 		return Couleur.values().length;
+	}
+
+	public String     toString()
+	{
+		return this.name();
 	}
 	
 	public static Couleur valueOf(int ordinal)
