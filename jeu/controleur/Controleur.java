fdb7213d2eafe1b3cb2f1e9c8698e382209bd87c
Alexis Coffard <alexis.coffard@etu.univ-lehavre.fr>
2026-06-09 15:20:23 +0200

    Redimension des fenêtres et positionnement

--- jeu/controleur/Controleur.java
+++ jeu/controleur/Controleur.java
@@ -1,17 +1,47 @@
 package controleur;
 
 import ihm.FrameJeu;
+import ihm.FrameCarte;
+
+import java.awt.Dimension;
 
 //import metier.PlateauJeu;
 
 public class Controleur
 {
-	private FrameJeu        ihm;
+	private FrameJeu        frameJeu;
+	private FrameCarte	frameCarte;
 	//private PlateauJeu      metier;
+	
+	int l, lJeu, lCarte;
+	int h;
+	int xCarte, yCarte;
+	int xPlateau, yPlateau;
 	
 	public Controleur()
 	{
-		this.ihm = new FrameJeu(this);
+		this.frameJeu   = new FrameJeu(this);
+		this.frameCarte = new FrameCarte(this);
+		
+		Dimension tailleEcran = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		System.out.println(tailleEcran);
+
+		h = (int) tailleEcran.getHeight() - 100 - 100;
+		l = (int) tailleEcran.getWidth () - 335 - 20 - 335;
+		
+		lJeu   = (int) (l * 0.60);
+		lCarte = (int) (l * 0.40);
+		
+		this.frameJeu  .setSize(lJeu  , h);
+		this.frameCarte.setSize(lCarte, h);
+		
+		xCarte   = 335;
+		xPlateau = xCarte + lCarte + 20;
+		
+		yCarte   = yPlateau = 100;
+		
+		this.frameJeu  .setLocation(xPlateau, yPlateau);
+		this.frameCarte.setLocation(xCarte  , yCarte  );
 	}
 
 	//getters
