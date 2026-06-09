8854567ddade597cca45f8b8c0606c6adbcb3391
Alexis Coffard <alexis.coffard@etu.univ-lehavre.fr>
2026-06-09 09:31:10 +0200

    renommage dossier

---
+++ creation/controleur/Controleur.java
@@ -0,0 +1,89 @@
+package controleur;
+
+import ihm.FrameCreation;
+
+import metier.Acteur;
+import metier.Casting;
+import metier.Couleur;
+import metier.CreateurPlateau;
+import metier.Role;
+import metier.Zone;
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class Controleur
+{
+	private FrameCreation   ihm;
+	private CreateurPlateau metier;
+	
+	public Controleur()
+	{
+		this.ihm = new FrameCreation(this);
+	}
+
+	//getters
+	public int getNbCasting ()               { return Casting.getNbCasting    ();}
+	public int getNbRole    ()               { return Role.getNbRole          ();}
+
+	public Zone[][]          getTabZone()    {return metier.getTabZone        ();}  //position des zones
+	public ArrayList<Zone>   getLstZones()   {return metier.getLstZones       ();} //listes des zones créées
+	public Zone              getZoneActive() {return metier.getZoneActive     ();}
+
+	public String            getNomPlateau() { return metier.getNomPlateau    ();}
+	public int               getNbLigne()    { return metier.getNbLigne       ();}
+	public int               getNbColonne()  { return metier.getNbColonne     ();}
+	public int               getTailleCase() { return metier.getTailleCase    ();}
+
+	public Role[]            getTabRole()    {return this.metier.getTabRole   ();}
+	public Casting[]         getTabCasting() {return this.metier.getTabCasting();}
+
+	public int               getNbCouleur()  { return Couleur.getNbCouleur    ();}
+
+	public ArrayList<Acteur> getLstActeurs() {return this.metier.getLstActeurs();}
+	
+	public String            getCasting   (int indice)
+	{ 
+		return Casting.valueOf ( indice ).getLibelle(); //Retourne le Libéllé du casting dont l'indice est entré en paramètre
+	}
+	
+	public String            getRole      (int indice)
+	{
+		return Role.valueOf    ( indice ).getLibelle(); //Retourne le Libéllé du role dont l'indice est entré en paramètre
+	}
+
+	//page 1: panel de saisie
+	public void initCreateur(String nomPlateau, int nbLigne, int nbColonne, int tailleCase, Role[] tabRole, Casting[] tabCasting)
+	{
+		this.metier = new CreateurPlateau(nomPlateau, nbLigne, nbColonne, tailleCase, tabRole, tabCasting);
+	}
+
+	//page 2: panel de Zones
+	public boolean modifierZone       (int lig, int col, Zone zone)  { return this.metier.modifierZone(lig, col, zone);}
+
+	public void    effacerZone        (Zone zone)                    { this.metier.effacerZone(zone); }
+
+	public boolean changerCouleurZone (Couleur couleur, Zone zone)   { return this.metier.changerCouleurZone(couleur, zone);}
+
+	public void    zonePrecedente     ()                             { this.metier.zonePrecedente();}
+
+	public void    zoneSuivante       ()                             { this.metier.zoneSuivante();}
+
+	public void    nouvelleZone       ()                             { this.metier.nouvelleZone();}
+
+	//page 3: panel de Roles
+	public void    creerPlateau      ()                              { this.metier.CreerPlateau();}
+
+	public boolean ajouterActeur     (Role role, int posX, int posY) { return this.metier.ajouterActeur(role, posX, posY);}
+
+	public boolean supprimerActeur   (int posX, int posY)            { return this.metier.supprimerActeur(posX, posY);}
+
+	public void    setPrincipal      (Color c, Acteur a)             { this.metier.setPrincipal(c, a);}
+	
+	public boolean estCastingUtilise (Color c)                       { return this.metier.estCastingUtilise(c); }
+
+	public Acteur  getActeur         (int posX, int posY)            { return this.metier.getActeur(posX, posY); }
+
+	public boolean principalPresent  ()                              { return this.metier.principalPresent();}
+
+	public static void main (String[] args) { new Controleur(); } //Démarage de l'application
+}
---
+++ creation/ihm/FrameCreation.java
@@ -0,0 +1,62 @@
+package ihm;
+
+import controleur.Controleur;
+import java.awt.BorderLayout;
+import java.io.File;
+
+import javax.swing.*;
+
+public class FrameCreation extends JFrame
+{
+	private Controleur ctrl;
+	private JPanel[] tabPanel;
+
+	private JPanel panelActif;
+	
+	public FrameCreation (Controleur ctrl)
+	{
+		this.ctrl = ctrl;
+		this.setTitle("Création/Modification des plateaux");
+		this.setSize(600, 700);
+		this.setLayout(new BorderLayout());
+		
+		this.tabPanel	 = new JPanel[4];
+		
+		this.tabPanel[0] = new PanelAccueilCreation(ctrl, this, 0);
+		//this.tabPanel[1] = new PanelSaisieCreation (ctrl, this, 1);
+		//this.tabPanel[2] = new PanelSymbole(this, 2);
+
+		this.panelActif = this.tabPanel[0];
+		
+		this.add(this.panelActif, BorderLayout.CENTER);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+		File filePrincipal = new File("Plateau");
+		filePrincipal.mkdir();
+	}
+	
+	public void setPnl(JPanel pnl)
+	{
+		this.remove(this.panelActif);
+		this.panelActif = pnl;
+		this.add(this.panelActif);
+			
+		this.repaint();
+		this.revalidate();
+	}
+
+	public void creerPanelSaisie(int indice)
+	{
+		this.tabPanel[indice] = new PanelSaisieCreation(ctrl, this, indice);
+	}
+
+	public void creerPanelZone(int indice)
+	{
+		this.tabPanel[indice] = new PanelZone(this.ctrl, this, indice);
+		this.tabPanel[indice + 1] = new PanelSymbole(this.ctrl, this, indice + 1);
+	}
+	
+	public JPanel getPnl(int i) { return this.tabPanel[i]; }
+}
+
