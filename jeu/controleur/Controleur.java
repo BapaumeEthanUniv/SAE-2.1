a54c2f968d1f9b44232cebca3b17080ac7f5faf7
Ethan Bapaume <ethan.bapaume@etu.univ-lehavre.fr>
2026-06-09 14:37:13 +0200

    Ajout de initPlateau et getter dans le controleur

--- jeu/controleur/Controleur.java
+++ jeu/controleur/Controleur.java
@@ -1,20 +1,36 @@
 package controleur;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import ihm.FrameJeu;
+import metier.Acteur;
+import metier.Casting;
+import metier.PlateauJeu;
+import metier.Zone;
 
 //import metier.PlateauJeu;
 
 public class Controleur
 {
 	private FrameJeu        ihm;
-	//private PlateauJeu      metier;
+	private PlateauJeu      metier;
 	
 	public Controleur()
 	{
 		this.ihm = new FrameJeu(this);
 	}
 
+	public void initPlateau(File filePlateau) { metier = new PlateauJeu(filePlateau);}
+
 	//getters
+	public int                getNbLigne()    {return this.metier.getNbLigne();}
+	public int                getNbColonne()  {return this.metier.getNbColonne();}
+	public int                getTailleCase() {return this.metier.getTailleCase();}
+	public ArrayList<Casting> getLstCasting() {return this.metier.getLstCasting();}
+	public Zone[][]           getTabZone()    {return this.metier.getTabZone();}
+	public Casting[][]        getTabArrete()  {return this.metier.getTabArrete();}
+	public ArrayList<Acteur>  getLstActeurs() {return this.metier.getLstActeurs();}
 	
 	public static void main (String[] args) { new Controleur(); } //Démarrage de l'application
 }
