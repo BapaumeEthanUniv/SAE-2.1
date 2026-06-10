14503e75b94315f893a00b67012f457bedb9cb53
Armand Dupont <armand.dupont@etu.univ-lehafre.fr>
2026-06-10 09:42:41 +0200

    modification de ajouterArette

--- jeu/metier/Acteur.java
+++ jeu/metier/Acteur.java
@@ -119,8 +119,8 @@ public class Acteur
 		return "" + this.role + String.format("%03d", this.posX) + String.format("%03d", this.posY);
 	}
 
-	public void ajouterArette(PlateauJeu plateau, Acteur voisin, Casting casting)
+	public void ajouterArette(PlateauJeu plateau, Acteur voisin)
 	{
-		plateau.ajouterArette(this, voisin, casting);
+		plateau.ajouterArette(this, voisin);
 	}
 }
