9a42fc678ac8ddd33c62f00f4c270f1a45f97795
Alexis Coffard <alexis.coffard@etu.univ-lehavre.fr>
2026-06-10 15:19:40 +0200

    modif toString pour enlever les _

--- creation/metier/Couleur.java
+++ creation/metier/Couleur.java
@@ -6,7 +6,6 @@ public enum Couleur
 {
 
 	ROSE        ( "Rose"        , new Color (230,100,160)),
-	ROUGE       ( "Rouge"       , Color.RED) ,
 	ROUGE_CLAIR ( "Rouge Clair" , new Color (255,64 ,64 )),
 	BORDEAUX    ( "Bordeaux"    , new Color (109,7  ,26 )),
 	ORANGE      ( "Orange"      , new Color (255,149,9  )),
@@ -24,9 +23,9 @@ public enum Couleur
 	VIOLET_PALE ( "Violet Pâle" , new Color (230,180,255)),
 	BLEU_PALE   ( "Bleu Pâle"   , new Color (175,221,255)),
 	VERT_PALE   ( "Vert Pâle"   , new Color (175,255,190)),
-	ROSE_PALE   ( "Rose pale"   , new Color (255,180,240)),
+	ROSE_PALE   ( "Rose Pâle"   , new Color (255,180,240)),
 	BEIGE       ( "Beige"       , new Color (255,215,175)),
-	TURQUOISE   ( "turquoise"   , new Color (110,255,255)),
+	TURQUOISE   ( "Turquoise"   , new Color (110,255,255)),
 	GRIS_CLAIR  ( "Gris clair"  , new Color (200,200,200)),
 	GRIS        ( "Gris"        , new Color (150,150,150));
 	
@@ -41,18 +40,32 @@ public enum Couleur
 	/*----------*/
 	
 	//Getters :
-	public        Color      getCouleur()   {return couleur;}
-	public        String     getLib()       {return lib;}
+	public        Color      getCouleur()   {return this.couleur;}
+	public        String     getLib()       {return this.lib;}
 	public static int        getNbCouleur() {return Couleur.values().length;}
 
 	// autres méthodes :
 	public String toString()
 	{
-		return this.name();
+		return this.lib;
 	}
 	
 	public static Couleur valueOf(int ordinal)
 	{
 		return Couleur.values()[ordinal];
+	}
+	
+	public static Couleur couleurCorrespondante(Color color)
+	{
+		
+		Couleur[] lstCouleurs = Couleur.values();
+		for (Couleur c : lstCouleurs)
+		{
+			if (c.getCouleur().equals(color))
+			{
+				return c;
+			}
+		}
+		return null;
 	}
 }
